841bfbd0822925dbf64dcaa422102d35ea60628c
OSA Shunsuke <shunsuke-osa@cookpad.com>
2017-12-21 14:20:51 +0900

    Static mock

--- App.js
+++ App.js
@@ -10,15 +10,14 @@ import {
   Platform,
   StyleSheet,
   Text,
-  View
+  View,
+  ListView,
 } from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+const StatusBar = require('./components/StatusBar');
+const ActionButton = require('./components/ActionButton');
+const ListItem = require('./components/ListItem');
+const styles = require('./styles.js');
 
 const firebaseConfig = {
   apiKey: '',
@@ -30,38 +29,38 @@ const firebaseConfig = {
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 
 export default class App extends Component<{}> {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dataSource: new ListView.DataSource(
+        {
+          rowHasChanged: (row1, row2) => { return row1 !== row2 },
+        }
+      ),
+    };
+  }
+
+  componentDidMount() {
+    this.setState(
+      {
+        dataSource: this.state.dataSource.cloneWithRows([{title: 'Pizza'}])
+      }
+    );
+  }
+
+  _renderItem(item) {
+    return (
+      <ListItem item={item} onpress={ () => {} }/>
+    );
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
+        <StatusBar title="Grocery List"/>
+        <ListView dataSource={this.state.dataSource} renderRow={this._renderItem.bind(this)} style={styles.listView}/>
+        <ActionButton title="Add" onpress={ () => {""}}/>
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
